93da8df28116d8f6a133f66ec4d2d7060efbaad8
David Waltenburg <david@waltenburg.dk>
2024-11-04 16:17:04 +0100

    a

---
+++ Model.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Todo> Todos { get; set; }
+    public DbSet<Task> Tasks { get; set; }
+    public string DbPath { get; }
+
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "blogging.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+public class Todo
+{
+    public int TodoId { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+public class Task
+{
+    public int TaskId { get; set; }
+    public string Name { get; set; }
+    public List<Todo> Todos { get; set; } = new List<Todo>();
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+using var db = new BloggingContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+db.SaveChanges();
+
+// Read
+Console.WriteLine("Querying for a blog");
+var blog = db.Blogs
+    .OrderBy(b => b.BlogId)
+    .First();
+
+// Update
+Console.WriteLine("Updating the blog and adding a post");
+blog.Url = "https://devblogs.microsoft.com/dotnet";
+blog.Posts.Add(
+    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+db.SaveChanges();
+
+// Delete
+Console.WriteLine("Delete the blog");
+db.Remove(blog);
+db.SaveChanges();
+
+seedTasks();
+
+
+using (BloggingContext context = new())
+{
+    var tasks = context.Tasks.Include(Task => Task.Todos);
+    foreach (var task in tasks)
+    {
+        Console.WriteLine($"Task: {task.Name}");
+        foreach (var todo in task.Todos)
+        {
+            Console.WriteLine($"- {todo.Name}");
+        }
+    }
+}
+
+void seedTasks()
+{
+    var task = new Task { Name = "Produce software" };
+    db.Tasks.Add(task);
+
+    var todo1 = new Todo { Name = "Write code", IsComplete = false };
+    db.Todos.Add(todo1);
+    task.Todos.Add(todo1);
+
+    var todo2 = new Todo { Name = "Compile source", IsComplete = false };
+    db.Todos.Add(todo2);
+    task.Todos.Add(todo2);
+
+    var todo3 = new Todo { Name = "Test program", IsComplete = false };
+    db.Todos.Add(todo3);
+    task.Todos.Add(todo3);
+    db.SaveChanges();
+
+
+    var task2 = new Task { Name = "Brew coffee" };
+    db.Tasks.Add(task2);
+
+    var todo4 = new Todo { Name = "Pour water", IsComplete = false };
+    db.Todos.Add(todo4);
+    task2.Todos.Add(todo4);
+
+    var todo5 = new Todo { Name = "Pour coffee", IsComplete = false };
+    db.Todos.Add(todo5);
+    task2.Todos.Add(todo5);
+
+    var todo6 = new Todo { Name = "Turn on", IsComplete = false };
+    db.Todos.Add(todo6);
+    task2.Todos.Add(todo6);
+    db.SaveChanges();
+}
